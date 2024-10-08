0d9732d091ae50d79fd45d26f6511f642da11c59
Shubham Patel <vtshubham01@gmail.com>
2024-10-08 22:36:03 +0530

    ADD STEERING CONTROLS

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ class Car(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=(490, 820))
         self.vel_vector = pygame.math.Vector2(0.8, 0)
         self.angle = 0
+        self.rotation_vel = 5
+        self.direction = 0
     
     def update(self):
         self.drive()
@@ -29,7 +31,16 @@ class Car(pygame.sprite.Sprite):
             self.rect.center += self.vel_vector * 6
     
     def rotate(self):
+        if self.direction == 1:
+            self.angle -= self.rotation_vel
+            self.vel_vector.rotate_ip(self.rotation_vel)
+
+        if self.direction == -1:
+            self.angle += self.rotation_vel
+            self.vel_vector.rotate_ip(-self.rotation_vel)
+
         self.image = pygame.transform.rotozoom(self.original_image, self.angle, 0.1)
+        self.rect = self.image.get_rect(center=self.rect.center)
 
 car = pygame.sprite.GroupSingle(Car())
 
@@ -47,10 +58,17 @@ def eval_genomes():
         user_input = pygame.key.get_pressed()
         if sum(pygame.key.get_pressed())<=1:
             car.sprite.drive_state = False
+            car.sprite.direction = 0
         
         #DRIVE
         if user_input[pygame.K_UP]:
             car.sprite.drive_state = True
+
+        #STEERING
+        if user_input[pygame.K_RIGHT]:
+            car.sprite.direction = 1
+        if user_input[pygame.K_LEFT]:
+            car.sprite.direction = -1
 
         #UPDATE
         car.draw(SCREEN)
